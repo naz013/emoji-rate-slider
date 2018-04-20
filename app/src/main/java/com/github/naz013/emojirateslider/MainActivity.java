2038941e2b18dafb0364e16436202b805e496d68
Nazar <n.suhovich@gmail.com>
2018-04-20 12:06:24 +0300

    Update sample.

--- app/src/main/java/com/github/naz013/emojirateslider/MainActivity.java
+++ app/src/main/java/com/github/naz013/emojirateslider/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.naz013.emojislider.EmojiRateSlider;
 
@@ -16,10 +17,17 @@ public class MainActivity extends AppCompatActivity implements EmojiRateSlider.O
 
         EmojiRateSlider slider = findViewById(R.id.mood_slider);
         slider.setOnMoodChangeListener(this);
+
+        EmojiRateSlider slider1 = findViewById(R.id.mood_slider2);
+        slider1.setOnMoodChangeListener(this);
+
+        EmojiRateSlider slider2 = findViewById(R.id.mood_slider3);
+        slider2.setOnMoodChangeListener(this);
     }
 
     @Override
     public void onMoodChanged(View view, int mood) {
         Log.d("MainActivity", "onMoodChanged: " + mood);
+        Toast.makeText(this, "Selected " + mood, Toast.LENGTH_SHORT).show();
     }
 }
